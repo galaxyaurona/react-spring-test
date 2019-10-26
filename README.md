c73f3afae7661150b6e580a43d917e992236a691
Nghia Tran <galaxyaurona@gmail.com>
2019-10-27 00:25:59 +1100

    Add springs for counter and usePrevious hook

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-#React Spring Test
+# React Spring Test
 
 This is an experimental project to test out the implementation of these feature
-- React Hooks
+- [React Hooks](https://reactjs.org/docs/hooks-intro.html)
 - [React Spring](https://www.react-spring.io/docs/hooks/use-trail)
-- Material UI
+- [Material UI](https://material-ui.com/)
 
-TODO: write more readme
+TODO: Update this readme when the experiment with more react related tech
--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,9 @@
 import React, {useState, useEffect} from 'react'
-
+import {useSpring, animated} from 'react-spring'
+import usePrevious from './hooks/usePrevious';
+function roundTo(num,decimalPoint){
+    return Math.round(num*10**decimalPoint)/10**decimalPoint
+}
 function Counter(props) {
     const [count, setCount] = useState(0)
     // be cause use effect to perform setup and cleanup only once
@@ -20,18 +24,16 @@ function Counter(props) {
     useEffect(()=>{
         countRef.current = count
     },[count])
-
+    const previousCount = usePrevious(count);
+    console.log(previousCount)
+    const animatedCount = useSpring({ config: {precision: 0.01}, value: roundTo(count,2) || 0, from: { value: roundTo(previousCount,2) || 0 } })
  
-    useEffect(()=>{
-        return () => {
-     
-        }
-    },[count])
     return (
         <div>
             <h1>Build in hooks</h1>
             <h2>Set state</h2>
-            <h3>Current count: {count}</h3>
+        
+            <h3>Current count:  <animated.span>{animatedCount.value}</animated.span></h3>
             <button onClick={()=> setCount(count+1)}>Increase</button>
             <button onClick={()=> setCount(count-1)}>Decrease</button>
         </div>
